08d235bacd74347867d3b176b209fa25afba2c8a
github-action-benchmark <github@users.noreply.github.com>
2022-04-10 10:44:46 +0000

    add Orchard Benchmarks (cargo) benchmark result for 330f492cb0348005a1f9abaffd54fec5b5b50649

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1649587485873,
+  "repoUrl": "https://github.com/QED-it/orchard",
+  "entries": {
+    "Orchard Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "jack@electriccoin.co",
+            "name": "str4d",
+            "username": "str4d"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "330f492cb0348005a1f9abaffd54fec5b5b50649",
+          "message": "Merge pull request #312 from zcash/release-0.1.0-beta.3\n\nRelease 0.1.0-beta.3",
+          "timestamp": "2022-04-06T21:48:37+01:00",
+          "tree_id": "b91119fca8253b3051b0fe5fbd4e15d090f2846f",
+          "url": "https://github.com/QED-it/orchard/commit/330f492cb0348005a1f9abaffd54fec5b5b50649"
+        },
+        "date": 1649587485086,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "proving/bundle/1",
+            "value": 5115882467,
+            "range": "± 25949416",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/2",
+            "value": 5124923629,
+            "range": "± 109849881",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/3",
+            "value": 7329656898,
+            "range": "± 27655435",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/4",
+            "value": 9543572832,
+            "range": "± 23533717",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/1",
+            "value": 39317832,
+            "range": "± 310880",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/2",
+            "value": 39621810,
+            "range": "± 616194",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/3",
+            "value": 44654709,
+            "range": "± 9050288",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/4",
+            "value": 48477583,
+            "range": "± 256655",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
