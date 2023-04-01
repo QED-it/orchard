f27c60cde676811820c3920c932d8a5b862ada38
alexeykoren <alexey.koren@gmail.com>
2023-04-01 22:14:46 +0200

    Use correct IK in proptest

--- src/issuance.rs
+++ src/issuance.rs
@@ -1049,6 +1049,7 @@ pub mod testing {
     use proptest::prelude::*;
     use proptest::prop_compose;
     use rand::{rngs::StdRng, SeedableRng};
+    use crate::keys::IssuanceValidatingKey;
 
     prop_compose! {
         /// Generate an issue action
@@ -1108,7 +1109,7 @@ pub mod testing {
             let rng = StdRng::from_seed(rng_seed);
 
             IssueBundle {
-                isk.ik,
+                ik: IssuanceValidatingKey::from(&isk),
                 actions,
                 authorization: Prepared { sighash: fake_sighash },
             }.sign(rng, &isk).unwrap()
