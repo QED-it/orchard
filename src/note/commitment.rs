a7e70ad919a9c6883c2930b56fb8d00f8fdbe197
Constance <constance@qed-it.com>
2023-04-03 10:26:43 +0200

    Constant-time note commitment

--- src/note/commitment.rs
+++ src/note/commitment.rs
@@ -3,12 +3,14 @@ use core::iter;
 use bitvec::{array::BitArray, order::Lsb0};
 use group::ff::{PrimeField, PrimeFieldBits};
 use halo2_gadgets::sinsemilla::primitives as sinsemilla;
+use halo2_gadgets::sinsemilla::primitives::append_hash_to_point;
 use pasta_curves::pallas;
 use subtle::{ConstantTimeEq, CtOption};
 
 use crate::{
     constants::{
         fixed_bases::{NOTE_COMMITMENT_PERSONALIZATION, NOTE_ZSA_COMMITMENT_PERSONALIZATION},
+        sinsemilla::K,
         L_ORCHARD_BASE,
     },
     note::asset_base::AssetBase,
@@ -56,35 +58,70 @@ impl NoteCommitment {
         let rho_bits = rho.to_le_bits();
         let psi_bits = psi.to_le_bits();
 
-        let zec_note_bits = iter::empty()
+        let remaining_bits =
+            (g_d_bits.len() + pk_d_bits.len() + v_bits.len() + 2 * L_ORCHARD_BASE) % K;
+
+        let (psi_bits_left, psi_bits_right) = psi_bits.split_at(L_ORCHARD_BASE - remaining_bits);
+
+        let common_bits = iter::empty()
             .chain(g_d_bits.iter().by_vals())
             .chain(pk_d_bits.iter().by_vals())
             .chain(v_bits.iter().by_vals())
             .chain(rho_bits.iter().by_vals().take(L_ORCHARD_BASE))
-            .chain(psi_bits.iter().by_vals().take(L_ORCHARD_BASE));
+            .chain(psi_bits_left.iter().by_vals());
 
-        // TODO: make this constant-time.
-        if asset.is_native().into() {
-            // Commit to ZEC notes as per the Orchard protocol.
-            Self::commit(NOTE_COMMITMENT_PERSONALIZATION, zec_note_bits, rcm)
-        } else {
-            // Commit to non-ZEC notes as per the ZSA protocol.
-            // Append the note type to the Orchard note encoding.
-            let type_bits = BitArray::<_, Lsb0>::new(asset.to_bytes());
-            let zsa_note_bits = zec_note_bits.chain(type_bits.iter().by_vals());
+        let zec_suffix = psi_bits_right.clone().iter().by_vals().take(remaining_bits);
+        let type_bits = BitArray::<_, Lsb0>::new(asset.to_bytes());
+        let zsa_suffix = iter::empty()
+            .chain(psi_bits_right.iter().by_vals().take(remaining_bits))
+            .chain(type_bits.iter().by_vals());
 
-            // Commit in a different domain than Orchard notes.
-            Self::commit(NOTE_ZSA_COMMITMENT_PERSONALIZATION, zsa_note_bits, rcm)
-        }
+        Self::double_constant_time_commit(
+            NOTE_COMMITMENT_PERSONALIZATION,
+            NOTE_ZSA_COMMITMENT_PERSONALIZATION,
+            common_bits,
+            zec_suffix,
+            zsa_suffix,
+            rcm,
+            asset.is_native().into(),
+        )
     }
 
-    fn commit(
-        personalization: &str,
-        bits: impl Iterator<Item = bool>,
+    /// Evaluates `SinsemillaCommit_{rcm}(personalization1, common_bits||suffix1)` and
+    /// `SinsemillaCommit_{rcm}(personalization2, common_bits||suffix2)` and returns the commit
+    /// corresponding to the choice.
+    ///
+    /// We would like to have a constant time implementation even if suffix1 and suffix2 have not
+    /// the same length.
+    /// `common_bits` must be a multiple of K bits
+    fn double_constant_time_commit(
+        personalization1: &str,
+        personalization2: &str,
+        common_bits: impl Iterator<Item = bool>,
+        suffix1: impl Iterator<Item = bool>,
+        suffix2: impl Iterator<Item = bool>,
         rcm: NoteCommitTrapdoor,
+        choice: bool,
     ) -> CtOption<Self> {
-        let domain = sinsemilla::CommitDomain::new(personalization);
-        domain.commit(bits, &rcm.0).map(NoteCommitment)
+        // Select the desired personalization
+        let domain = if choice {
+            sinsemilla::CommitDomain::new(personalization1)
+        } else {
+            sinsemilla::CommitDomain::new(personalization2)
+        };
+        // Evaluate the hash on the `common_bits`
+        let common_hash = domain.hash_to_point_inner(common_bits);
+        // Continue to evaluate the hash from the previous hash with each possible suffix
+        // We would like to have a constant time implementation. Hence, we have to evaluate the
+        // hash for the both suffixes
+        let hash1 = append_hash_to_point(common_hash, suffix1);
+        let hash2 = append_hash_to_point(common_hash, suffix2);
+        // Select the desired hash
+        let note_hash = if choice { hash1 } else { hash2 };
+        // Evaluate the commitment from this hash point
+        domain
+            .commit_from_hash_point(note_hash, &rcm.0)
+            .map(NoteCommitment)
     }
 }
 
