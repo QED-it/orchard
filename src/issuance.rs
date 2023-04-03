2b17ed0a15b804eee946df105372af0b565d53b6
alexeykoren <alexey.koren@gmail.com>
2023-04-03 23:21:51 +0200

    Rebase

--- src/issuance.rs
+++ src/issuance.rs
@@ -1285,7 +1285,6 @@ pub mod testing {
     use proptest::collection::vec;
     use proptest::prelude::*;
     use proptest::prop_compose;
-    use rand::{rngs::StdRng, SeedableRng};
     use reddsa::orchard::SpendAuth;
     use crate::primitives::redpallas::Signature;
 
