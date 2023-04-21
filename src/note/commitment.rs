103485ab2d469b31e4f4f0bafc71ea0b95e306c3
Constance <constance@qed-it.com>
2023-04-21 10:51:09 +0200

    Compute the mutual part once

--- src/note/commitment.rs
+++ src/note/commitment.rs
@@ -56,20 +56,19 @@ impl NoteCommitment {
         let rho_bits = rho.to_le_bits();
         let psi_bits = psi.to_le_bits();
 
-        let zec_note_bits = iter::empty()
-            .chain(g_d_bits.iter().by_vals())
-            .chain(pk_d_bits.iter().by_vals())
-            .chain(v_bits.iter().by_vals())
-            .chain(rho_bits.iter().by_vals().take(L_ORCHARD_BASE))
-            .chain(psi_bits.iter().by_vals().take(L_ORCHARD_BASE));
-
-        let type_bits = BitArray::<_, Lsb0>::new(asset.to_bytes());
-        let zsa_note_bits = iter::empty()
+        let common_note_bits = iter::empty()
             .chain(g_d_bits.iter().by_vals())
             .chain(pk_d_bits.iter().by_vals())
             .chain(v_bits.iter().by_vals())
             .chain(rho_bits.iter().by_vals().take(L_ORCHARD_BASE))
             .chain(psi_bits.iter().by_vals().take(L_ORCHARD_BASE))
+            .collect::<Vec<bool>>();
+
+        let zec_note_bits = common_note_bits.clone().into_iter();
+
+        let type_bits = BitArray::<_, Lsb0>::new(asset.to_bytes());
+        let zsa_note_bits = common_note_bits
+            .into_iter()
             .chain(type_bits.iter().by_vals());
 
         let zec_domain = sinsemilla::CommitDomain::new(NOTE_COMMITMENT_PERSONALIZATION);
