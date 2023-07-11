b3df8008584f72fb06f28d1ed5bb364a7e88bfd5
Constance <constance@qed-it.com>
2023-07-11 14:59:47 +0200

    Update comment

--- src/circuit.rs
+++ src/circuit.rs
@@ -279,7 +279,7 @@ impl plonk::Circuit<pallas::Base> for Circuit {
                     ),
                     // We already checked that
                     // * is_native_asset is boolean (just below), and
-                    // * v_old is a 64 bit integer (in the note commitment evaluation).
+                    // * v_old is a 64 bit unsigned integer (in the note commitment evaluation).
                     // So, 1 - is_native_asset + v_old = 0 only when (is_native_asset = 1 and v_old = 0), no overflow can occur.
                     (
                         "(v_old = 0 and is_native_asset = 1) or (root = anchor)",
