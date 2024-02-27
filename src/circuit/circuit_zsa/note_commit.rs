b9f28785e22ba63fb190a48207ec6fb4cb20927f
Dmitry Demin <dmitry@qed-it.com>
2024-02-27 07:14:47 +0100

    Fix to support modified halo2

--- src/circuit/circuit_zsa/note_commit.rs
+++ src/circuit/circuit_zsa/note_commit.rs
@@ -2407,7 +2407,7 @@ mod tests {
                     table_idx,
                     meta.lookup_table_column(),
                     meta.lookup_table_column(),
-                    table_range_check_tag,
+                    Some(table_range_check_tag),
                 );
                 let lagrange_coeffs = [
                     meta.fixed_column(),
@@ -2424,7 +2424,7 @@ mod tests {
                     meta,
                     advices[9],
                     table_idx,
-                    table_range_check_tag,
+                    Some(table_range_check_tag),
                 );
                 let sinsemilla_config = SinsemillaChip::<
                     OrchardHashDomains,
