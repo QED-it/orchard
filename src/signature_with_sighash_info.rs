6803f6ca586ee24cd865d46819edb3f79bf3f2ba
Constance Beguier <constance@qed-it.com>
2025-08-14 18:36:58 +0200

    Create the generic structure SignatureWithSighashInfo

--- src/signature_with_sighash_info.rs
+++ src/signature_with_sighash_info.rs
@@ -2,7 +2,7 @@
 
 use alloc::vec::Vec;
 
-use crate::primitives::redpallas::{self, Binding, SpendAuth};
+use crate::primitives::redpallas::{self, Binding, SigType, SpendAuth};
 
 /// The sighash version and associated information
 #[derive(Debug, Clone)]
@@ -17,58 +17,33 @@ pub(crate) const ORCHARD_SIG_V0: SighashInfo = SighashInfo {
     associated_information: vec![],
 };
 
-/// Binding signature containing the sighash information and the signature itself.
 #[derive(Debug, Clone)]
-pub struct BindingSignatureWithSighashInfo {
-    /// The sighash information for the binding signature.
+pub struct SignatureWithSighashInfo<T: SigType> {
     info: SighashInfo,
-    /// The binding signature.
-    signature: redpallas::Signature<Binding>,
+    signature: redpallas::Signature<T>,
 }
 
-impl BindingSignatureWithSighashInfo {
-    pub(crate) fn new(info: SighashInfo, signature: redpallas::Signature<Binding>) -> Self {
-        BindingSignatureWithSighashInfo { info, signature }
+impl<T: SigType> SignatureWithSighashInfo<T> {
+    pub(crate) fn new(info: SighashInfo, signature: redpallas::Signature<T>) -> Self {
+        Self { info, signature }
     }
 
     pub(crate) fn to_bytes(&self) -> Vec<u8> {
-        [self.info.version]
-            .into_iter()
-            .chain(self.info.associated_information.clone().into_iter())
-            .chain(<[u8; 64]>::from(&self.signature).into_iter())
-            .collect::<Vec<u8>>()
+        let mut result = Vec::with_capacity(1 + self.info.associated_information.len() + 64);
+        result.push(self.info.version);
+        result.extend_from_slice(&self.info.associated_information);
+        result.extend_from_slice(&<[u8; 64]>::from(&self.signature));
+        result
     }
 
-    /// Returns the binding signature.
-    pub fn signature(&self) -> &redpallas::Signature<Binding> {
+    /// Returns the signature.
+    pub fn signature(&self) -> &redpallas::Signature<T> {
         &self.signature
     }
 }
+
+/// Binding signature containing the sighash information and the signature itself.
+pub type BindingSignatureWithSighashInfo = SignatureWithSighashInfo<Binding>;
 
 /// Authorizing signature containing the sighash information and the signature itself.
-#[derive(Debug, Clone)]
-pub struct SpendAuthSignatureWithSighashInfo {
-    /// The sighash information for the authorizing signature.
-    info: SighashInfo,
-    /// The authorizing signature.
-    signature: redpallas::Signature<SpendAuth>,
-}
-
-impl SpendAuthSignatureWithSighashInfo {
-    pub(crate) fn new(info: SighashInfo, signature: redpallas::Signature<SpendAuth>) -> Self {
-        SpendAuthSignatureWithSighashInfo { info, signature }
-    }
-
-    pub(crate) fn to_bytes(&self) -> Vec<u8> {
-        [self.info.version]
-            .into_iter()
-            .chain(self.info.associated_information.clone().into_iter())
-            .chain(<[u8; 64]>::from(&self.signature).into_iter())
-            .collect::<Vec<u8>>()
-    }
-
-    /// Returns the authorizing signature.
-    pub fn signature(&self) -> &redpallas::Signature<SpendAuth> {
-        &self.signature
-    }
-}
+pub type SpendAuthSignatureWithSighashInfo = SignatureWithSighashInfo<SpendAuth>;
