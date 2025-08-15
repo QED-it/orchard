6557bc562ce3f051898dc16cf5df00c5d1532143
Constance Beguier <constance@qed-it.com>
2025-08-15 14:14:49 +0200

    Update PCZT with SignatureWithSighashInfo

--- src/pczt.rs
+++ src/pczt.rs
@@ -16,10 +16,9 @@ use crate::{
     note::{
         AssetBase, ExtractedNoteCommitment, Nullifier, RandomSeed, Rho, TransmittedNoteCiphertext,
     },
-    primitives::{
-        redpallas::{self, Binding, SpendAuth},
-        OrchardPrimitives,
-    },
+    primitives::redpallas::{self, Binding, SpendAuth},
+    primitives::OrchardPrimitives,
+    signature_with_sighash_info::SpendAuthSignatureWithSighashInfo,
     tree::MerklePath,
     value::{NoteValue, ValueCommitTrapdoor, ValueCommitment, ValueSum},
     Address, Anchor, Proof,
@@ -161,7 +160,7 @@ pub struct Spend {
     /// The spend authorization signature.
     ///
     /// This is set by the Signer.
-    pub(crate) spend_auth_sig: Option<redpallas::Signature<SpendAuth>>,
+    pub(crate) spend_auth_sig: Option<SpendAuthSignatureWithSighashInfo>,
 
     /// The address that received the note being spent.
     ///
