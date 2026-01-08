41a0f90a40d30ed50339410f63f4039b7fa60d42
Constance Beguier <constance@qed-it.com>
2026-01-08 12:10:54 +0100

    Fix error message

--- tests/zsa.rs
+++ tests/zsa.rs
@@ -598,7 +598,7 @@ fn zsa_issue_and_transfer() {
     );
     match result {
         Ok(_) => panic!("Test should fail"),
-        Err(error) => assert_eq!(error, "Burning is only possible for non-native assets"),
+        Err(error) => assert_eq!(error, "Burn error: Cannot burn a native asset."),
     }
 
     // 12. Try to burn zero value - should fail
@@ -616,6 +616,6 @@ fn zsa_issue_and_transfer() {
     );
     match result {
         Ok(_) => panic!("Test should fail"),
-        Err(error) => assert_eq!(error, "Burning is not possible for zero values"),
+        Err(error) => assert_eq!(error, "Burn error: Cannot burn an asset with a zero value."),
     }
 }
