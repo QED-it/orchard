b797bab9e893656fef8adfb75264513bb96d07fb
Constance Beguier <constance@qed-it.com>
2026-08-28 11:41:23 +0200

    Name the bench groups after their marker types

--- benches/utils/mod.rs
+++ benches/utils/mod.rs
@@ -46,7 +46,7 @@ impl OrchardFlavorBench for IronwoodV3 {
         c: &'a mut Criterion<M>,
         group_name: &str,
     ) -> BenchmarkGroup<'a, M> {
-        c.benchmark_group(format!("[Ironwood] {}", group_name))
+        c.benchmark_group(format!("[IronwoodV3] {}", group_name))
     }
 }
 
@@ -58,6 +58,6 @@ impl OrchardFlavorBench for Zsa {
         c: &'a mut Criterion<M>,
         group_name: &str,
     ) -> BenchmarkGroup<'a, M> {
-        c.benchmark_group(format!("[OrchardZSA] {}", group_name))
+        c.benchmark_group(format!("[Zsa] {}", group_name))
     }
 }
